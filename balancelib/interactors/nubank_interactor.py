8827c11271a0039859c373f2e4a07e34bfb0282c
Calenzo <calenzodevs@gmail.com>
2023-03-31 21:49:21 -0300

    fix: I was changed the local of save file certificate_url

--- balancelib/interactors/nubank_interactor.py
+++ balancelib/interactors/nubank_interactor.py
@@ -33,7 +33,7 @@ class NuBankInteractor(NuBankServiceBasicInterface):
 
         if has_file:
             # s3.download_file(bucket_fastapi, 'api.zip', 'api.zip')
-            s3.download_file(bucket_certificates, certificate_url, certificate_url)
+            s3.download_file(bucket_certificates, certificate_url, f'tmp/{certificate_url}')
             #
             # with zipfile.ZipFile('api.zip', mode='a') as package:
             #     package.write(certificate_url, arcname=certificate_url)
